124e4c268a2059a70c9779266393cfaec7e580f1
Veacky <valerian.grandjean@gmail.com>
2018-10-25 17:51:15 +0300

    Ok

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// ToDo : Vérifier que la connexion est OK
-$db = new PDO("mysql:host=127.0.0.1:51033;port=5433;dbname=localdb","azure","6#vWHD_$");
-
-// Pour éviter d'avoir à réutiliser "global" par la suite
-function db() { global $db; return $db; }
-
-mdr 
