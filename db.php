c8da99ae99a15ffcdd5c212f298366fe06fba835
Veacky <valerian.grandjean@gmail.com>
2018-10-25 17:49:02 +0300

    t

--- db.php
+++ db.php
@@ -5,3 +5,5 @@ $db = new PDO("mysql:host=127.0.0.1:51033;port=5433;dbname=localdb","azure","6#v
 
 // Pour éviter d'avoir à réutiliser "global" par la suite
 function db() { global $db; return $db; }
+
+mdr 
